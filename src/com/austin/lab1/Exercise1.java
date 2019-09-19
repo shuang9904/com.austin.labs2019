f4b8599b98aa20d345a5b54c2b3461ab5f8134ff
t00204308 <shuang9904@gmail.com>
2019-09-19 11:47:29 +0100

    code1 uodate

--- src/com/austin/lab1/Exercise1.java
+++ src/com/austin/lab1/Exercise1.java
@@ -2,6 +2,6 @@ package com.austin.lab1;
 
 public class Exercise1 {
     public static void main(String[] args) {
-        System.out.print("hello");
+        System.out.print("hellsdfsfsfo");
     }
 }
